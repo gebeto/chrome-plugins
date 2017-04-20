b58ff64a95d6ae7c84d541127d4babc12403b3ea
gebeto <bboyheadman@gmail.com>
2017-04-20 15:06:22 +0300

    QuickCommit

--- kinogo-ad-remover/js/content.js
+++ kinogo-ad-remover/js/content.js
@@ -1,15 +1,16 @@
 ;(function() {
 
   "use strict" ;
+  
+  setTimeout(function() {
+    var noindex = document.getElementsByTagName('noindex');
 
-  var noindex = document.getElementsByTagName('noindex');
-
-  for (var i = 0; i < noindex.length; i++) {
-    if (noindex[i].innerHTML.indexOf('http://kinogo.club') < 0 || noindex[i].innerHTML.indexOf('iframe') > -1) {
-      console.log(noindex[i]);
-      noindex[i].style.display = 'none';
+    for (var i = 0; i < noindex.length; i++) {
+      if (noindex[i].innerHTML.indexOf('http://kinogo.club') < 0 || noindex[i].innerHTML.indexOf('iframe') > -1) {
+        console.log(noindex[i]);
+        noindex[i].style.display = 'none';
+      }
     }
-    // noindex[i].style.display = 'none';
-  }
+  // }, 1000);
 
 })();
