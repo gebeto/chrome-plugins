59fe0a2e129bcb69f94e4cca964c252a196992ad
gebeto <slavik.nychkalo@gmail.com>
2017-11-28 15:58:50 +0200

    upd

--- nulp-timer/main_end.js
+++ nulp-timer/main_end.js
@@ -18,16 +18,39 @@ var shedule = {
 	var table = document.querySelector('table.outer');
 
 	if (table) {
-		console.log(table);
-		var cells = [...table.querySelectorAll('tbody>tr>td[class=leftcell]')].filter((cell) => cell.textContent.match(/\d/));
-		console.log(cells);
-		cells.map((cell) => {
-			var cellContent = shedule[cell.textContent];
-			cell.title = cellContent;
-			cell.parentNode.title = cellContent;
-			cell.style.cursor = 'pointer';
-			cell.parentNode.innerHTML += `<td class="leftcell" style="text-align: center"><small><small><small>${cellContent.replace(' - ', '<br/>-<br/>')}</small></small></small></td>`;
-		});
+		(function normalTable(){
+			console.log(table);
+			var cells = [...table.querySelectorAll('tbody>tr>td[class=leftcell]')].filter((cell) => cell.textContent.match(/\d/));
+			console.log(cells);
+			cells.map((cell) => {
+				var cellContent = shedule[cell.textContent];
+				cell.title = cellContent;
+				cell.parentNode.title = cellContent;
+				cell.style.cursor = 'pointer';
+				cell.parentNode.innerHTML += `<td class="leftcell" style="text-align: center"><small><small><small>${cellContent.replace(' - ', '<br/>-<br/>')}</small></small></small></td>`;
+			});
+		})();
+
+		(function zaochka(){
+			var cells = [...table.querySelectorAll('.cell1')].filter((cell) => cell.textContent.match(/\d/));
+			cells.map((cell) => {
+				var cellContent = shedule[cell.textContent];
+				cell.title = cellContent;
+				cell.parentNode.title = cellContent;
+				cell.style.cursor = 'pointer';
+				cell.parentNode.innerHTML += `<td class="cell5" style="text-align: center"><small><small>${cellContent.replace(' - ', '<br/>-<br/>')}</small></small></td>`;
+			});
+
+			if (cells) {
+				[...document.querySelectorAll('.data')].map((elem) => {
+					elem.colSpan = 6;
+				});
+				
+				[...document.querySelectorAll('.zagol1')].map((elem) => {
+					elem.parentNode.innerHTML += '<th class="zagol5">Час</th>';
+				});
+			}
+		})();
 	}
 
 })();
